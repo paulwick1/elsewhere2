dce6ec2fd8bc4050708f6d05b4337aac8245af2a
John Paul Wicklund <John Paul Wicklund>
2017-04-29 01:13:22 -0500

    bower

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var bower = require('bower');
  
 var app = express();
 var PORT = process.env.PORT || 8080;
